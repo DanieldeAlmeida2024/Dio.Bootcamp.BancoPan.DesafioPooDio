655f454d2a2ae92daa8daf1d8b92540039ed431e
Danieldealmeida2024 <danieldealmeidadev@outlook.com>
2025-02-21 17:43:55 -0300

    Mentoria e Curso

---
+++ src/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,62 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+    String titulo;
+    String descricao;
+    int cargaHoraria;
+    LocalDate data;
+
+    public Mentoria() {
+    }
+
+    public Mentoria(String titulo, String descricao, int cargaHoraria, LocalDate data) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+        this.data = LocalDate.now();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                ", data=" + data +
+                '}';
+    }
+}
